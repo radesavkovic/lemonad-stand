e775a22c1180027da578de9ba531ddc6527eebc5
Dusan Snegic <124081844+DusanSnegic@users.noreply.github.com>
2023-02-09 20:41:25 +0900

    Repair repository

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const config = {
+  chainId: 56,
+  // contractAddress: "0xa330Ae137B766694868158723489bDA96b0A55eF",
+  // chainId: 338,
+  contractAddress: "0x8be8881C641Dc5A40845253Ee3eD04955eDFe96D",
+  scanLink:
+    "https://www.bscscan.com/address/0x8be8881C641Dc5A40845253Ee3eD04955eDFe96D"
+};
